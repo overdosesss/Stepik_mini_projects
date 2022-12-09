4bb28c58ef47ee5ed10ebf01d623671ea5972e8a
overdosesss <tbboneez@gmail.com>
2022-12-09 22:02:17 +0300

    Upgrade

--- Num_guesser/main.py
+++ Num_guesser/main.py
@@ -1,17 +1,46 @@
 from random import randint
 
 
-# Сгенерируем случайное число из диапазона [1; 100]
-num_for_guess = randint(1, 100)
+# Функция для проверки ввода пользователя
+def user_input():
+    try:
+        num = int(input("Введите число:\n"))
 
-# Бесконечный цикл, где будет ввод данных от пользователя и проверка угадал ли он
-while True:
-    user_input = int(input("Введите число:\n"))
+        if 1 <= num <= 100:
+            return num
+        
+        raise Exception
+    except ValueError:
+        print("Вы ошиблись при вводе, попробуйте ещё раз...")
+        main()
+    except Exception:
+        print("Не попали в диапазон [1; 100], попробуйте ещё раз...")
+        main()
+    
 
-    if user_input > num_for_guess:
-        print("Промах — перебор.")
-    elif user_input < num_for_guess:
-        print("Промах — недобор.")
-    else:
-        print("Вы угадали!")
-        break
+
+# Приветствие
+print("Игра 'Угадай число от 1 до 100'.\nСейчас мы загадаем число, а ваша задача — отгадать его.\n")
+
+
+def main():
+    # Сгенерируем случайное число из диапазона [1; 100]
+    num_for_guess = randint(1, 100)
+
+    # Бесконечный цикл, где будет ввод данных от пользователя и проверка угадал ли он
+    while True:
+        # Получаем вариант пользователя
+        user_data = user_input()
+
+        # Сравниваем с загаданным числом и выводим результат
+        if user_data > num_for_guess:
+            print("Промах — перебор.\n")
+        elif user_data < num_for_guess:
+            print("Промах — недобор.\n")
+        else:
+            print("Вы угадали!\n")
+            break
+
+
+if __name__ == "__main__":
+    main()
